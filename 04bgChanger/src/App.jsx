9632b9dd7e3825439652bed372a1c62d1ae6feb4
tanya <tanyamaurya1232gmail.com>
2024-01-29 20:32:04 +0530

    coomit

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ">
+          <button
+          //onClick ={() => {}}  //call back is a funcn
+          //onClick={setColor}  kaam hojta par onclick expects you to pass a function not a reference cz you want to pass a parameter so that's why use down method
+          onClick={() => setColor("red")} //function k andar funcn call kr re hain
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+
+          <button
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
